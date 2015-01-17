4a886aadddc7e2f7776a98ca90f69cb171b2a505
nichwch <nichwch@gmail.com>
2015-01-17 10:35:35 -0800

    1

--- game.js
+++ game.js
@@ -3,9 +3,51 @@ var button1=document.getElementById("button1");
 var button2=document.getElementById("button2");
 var button3=document.getElementById("button3");
 var button4=document.getElementById("button4");
-
-
+var selection=0;
+var random=0;
+var question;
+var questionprompt;
+var answer;
 var quiz=function()
 {
-  
+  random=Math.floor((Math.random()*4));
+  question=questions[random];
+  questionprompt=question[0];
+  answer=question[5];
+  prompt.innerHTML=questionprompt;
+  button1.innerHTML=question[1];
+  button2.innerHTML=question[2];
+  button3.innerHTML=question[3];
+  button4.innerHTML=question[4];
+  var handleOne = function(event){
+    selection=1;
+    checkAnswer();
+  }
+  var handleTwo = function(event){
+    selection=2;
+    checkAnswer();
+  }
+  var handleThree = function(event){
+    selection=3;
+    checkAnswer();
+  }
+  var handleFour = function(event){
+    selection=4;
+    checkAnswer();
+  }
+  var checkAnswer = function(){
+    if(selection=answer)
+    {
+
+    }
+    else
+    {
+      
+    }
+  }
+  button1.addEventListener('onClick',handleOne);
+  button2.addEventListener('onClick',handleTwo);
+  button3.addEventListener('onClick',handleThree);
+  button4.addEventListener('onClick',handleFour);
 }
+quiz();
--- questions.js
+++ questions.js
@@ -1,4 +1,7 @@
 var questions=
-[["How do you say hello in spanish?","Bonjour","sup","yo","hola"],
-["How do you win a hackathon?","Fork git projects","drink coffee","drink mountain dew","be nicholas"]
+[
+["How do you say hello in spanish?","Bonjour","sup","yo","hola",4],
+["How do you win a hackathon?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4],
+["How do you win money?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4],
+["How do you eat food?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4]
 ];
