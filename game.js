bfd31aa62e03608cc25d12fc8fe7e2b3dcebe48b
nichwch <nichwch@gmail.com>
2015-01-17 08:58:50 -0800

    Uploaded changes

---
+++ game.js
@@ -0,0 +1,11 @@
+var prompt=document.getElementById("prompt");
+var button1=document.getElementById("button1");
+var button2=document.getElementById("button2");
+var button3=document.getElementById("button3");
+var button4=document.getElementById("button4");
+
+
+var quiz=function()
+{
+  
+}
---
+++ questions.js
@@ -0,0 +1,4 @@
+var questions=
+[["How do you say hello in spanish?","Bonjour","sup","yo","hola"],
+["How do you win a hackathon?","Fork git projects","drink coffee","drink mountain dew","be nicholas"]
+];
