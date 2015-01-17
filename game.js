3f5f06d8dc76383669143cd897b7f18f3ee60742
nichwch <nichwch@gmail.com>
2015-01-17 14:35:25 -0800

    asdf

--- game.js
+++ game.js
@@ -1,13 +1,65 @@
+    var robot = new Image();
+robot.src = "http://nichwch.github.io/nickbot.png";
+var canvas = document.getElementById("canvas");
+function sprite (options) {
+
+    var that = {};
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+   that.render = function()
+    {
+
+      that.context.drawImage(
+      that.image,
+      0,
+      0,
+      that.width,
+      that.height,
+      0,
+      0,
+      that.width,
+      that.height
+      );
+    }
+    return that;
+
+}
+var player = sprite({
+    context: canvas.getContext("2d"),
+    width: 100,
+    height: 100,
+    image: robot
+});
+
+player.render;
+function sprite (options) {
+    var that = {};
+
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+
+    return that;
+}
+
 var prompt=document.getElementById("prompt");
 var button1=document.getElementById("button1");
 var button2=document.getElementById("button2");
 var button3=document.getElementById("button3");
 var button4=document.getElementById("button4");
+var contbutton=document.getElementById("contbutton");
+var popup=document.getElementById("popup");
+var popupmessage=document.getElementById("popupmessage");
 var selection=0;
 var random=0;
 var question;
 var questionprompt;
 var answer;
+contbutton.style.visibility="hidden";
+popup.style.visibility="hidden";
 var quiz=function()
 {
   random=Math.floor((Math.random()*4));
@@ -36,18 +88,58 @@ var quiz=function()
     checkAnswer();
   }
   var checkAnswer = function(){
-    if(selection=answer)
+    if(selection==answer)
     {
-
+      var cont = function(event)
+      {
+         button1.style.visibility='visible';
+        button2.style.visibility='visible';
+        button3.style.visibility='visible';
+        button4.style.visibility='visible';
+         contbutton.style.visibility='hidden';
+         contbutton.removeEventListener('click',cont);
+          quiz();
+      }
+      contbutton.addEventListener('click',cont);
+      contbutton.style.visibility='visible';
+      button1.removeEventListener('click',handleOne);
+     button2.removeEventListener('click',handleTwo);
+      button3.removeEventListener('click',handleThree);
+      button4.removeEventListener('click',handleFour);
+      button1.style.visibility='hidden';
+      button2.style.visibility='hidden';
+      button3.style.visibility='hidden';
+      button4.style.visibility='hidden';
+      prompt.innerHTML="You got the right answer!";
     }
     else
     {
-      
+            var cont = function(event)
+      {
+         button1.style.visibility='visible';
+        button2.style.visibility='visible';
+        button3.style.visibility='visible';
+        button4.style.visibility='visible';
+         contbutton.style.visibility='hidden';
+         contbutton.removeEventListener('click',cont);
+          quiz();
+      }
+      contbutton.addEventListener('click',cont);
+      contbutton.style.visibility='visible';
+      button1.removeEventListener('click',handleOne);
+     button2.removeEventListener('click',handleTwo);
+      button3.removeEventListener('click',handleThree);
+      button4.removeEventListener('click',handleFour);
+      button1.style.visibility='hidden';
+      button2.style.visibility='hidden';
+      button3.style.visibility='hidden';
+      button4.style.visibility='hidden';
+      prompt.innerHTML="Sorry, that was incorrect. The correct answer is "+question[answer];
     }
   }
-  button1.addEventListener('onClick',handleOne);
-  button2.addEventListener('onClick',handleTwo);
-  button3.addEventListener('onClick',handleThree);
-  button4.addEventListener('onClick',handleFour);
+  button1.addEventListener('click',handleOne);
+  button2.addEventListener('click',handleTwo);
+  button3.addEventListener('click',handleThree);
+  button4.addEventListener('click',handleFour);
 }
 quiz();
