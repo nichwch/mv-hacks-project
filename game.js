15311a22dbb9e93d95fff57c4355fc3a7f989acc
nichwch <nichwch@gmail.com>
2015-01-17 18:26:45 -0800

    asdf

--- game.js
+++ game.js
@@ -1,51 +1,5 @@
 /*SWAG*/
 
-var robot = new Image();
-robot.src = "http://nichwch.github.io/nickbot.png";
-var canvas = document.getElementById("canvas");
-function sprite (options) {
-
-    var that = {};
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-   that.render = function()
-    {
-
-      that.context.drawImage(
-      that.image,
-      0,
-      0,
-      that.width,
-      that.height,
-      0,
-      0,
-      that.width,
-      that.height
-      );
-    }
-    return that;
-
-}
-var player = sprite({
-    context: canvas.getContext("2d"),
-    width: 100,
-    height: 100,
-    image: robot
-});
-
-player.render;
-function sprite (options) {
-    var that = {};
-
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-
-    return that;
-}
 
 var prompt=document.getElementById("prompt");
 var button1=document.getElementById("button1");
@@ -53,6 +7,8 @@ var button2=document.getElementById("button2");
 var button3=document.getElementById("button3");
 var button4=document.getElementById("button4");
 var contbutton=document.getElementById("contbutton");
+var explosion=document.getElementById("explosion");
+var laser=document.getElementById("laser");
 var popup=document.getElementById("popup");
 var popupmessage=document.getElementById("popupmessage");
 var selection=0;
@@ -62,6 +18,20 @@ var questionprompt;
 var answer;
 contbutton.style.visibility="hidden";
 popup.style.visibility="hidden";
+var animate=function(sho,vic)
+{
+  var shooter=sho.getBoundingClientRect();
+  var victim=vic.getBoundingClientRect();
+  laser.style.visibility="visible";
+  laser.style.left=(shooter.left)-(shooter.width);
+  laser.style.top=(shooter.top)-(shooter.height);
+  while(laser.style.left<vic.style.left)
+  {
+    laser.style.left=laser.style.left+1;
+  }
+}
+
+
 var quiz=function()
 {
   random=Math.floor((Math.random()*4));
@@ -93,55 +63,55 @@ var quiz=function()
     if(selection==answer)
     {
       var cont = function(event)
-      {
-         button1.style.visibility='visible';
-        button2.style.visibility='visible';
-        button3.style.visibility='visible';
-        button4.style.visibility='visible';
-         contbutton.style.visibility='hidden';
-         contbutton.removeEventListener('click',cont);
-          quiz();
-      }
-      contbutton.addEventListener('click',cont);
-      contbutton.style.visibility='visible';
-      button1.removeEventListener('click',handleOne);
-     button2.removeEventListener('click',handleTwo);
-      button3.removeEventListener('click',handleThree);
-      button4.removeEventListener('click',handleFour);
-      button1.style.visibility='hidden';
-      button2.style.visibility='hidden';
-      button3.style.visibility='hidden';
-      button4.style.visibility='hidden';
-      prompt.innerHTML="You got the right answer!";
-    }
-    else
     {
-            var cont = function(event)
-      {
-         button1.style.visibility='visible';
-        button2.style.visibility='visible';
-        button3.style.visibility='visible';
-        button4.style.visibility='visible';
-         contbutton.style.visibility='hidden';
-         contbutton.removeEventListener('click',cont);
-          quiz();
-      }
-      contbutton.addEventListener('click',cont);
-      contbutton.style.visibility='visible';
-      button1.removeEventListener('click',handleOne);
-     button2.removeEventListener('click',handleTwo);
-      button3.removeEventListener('click',handleThree);
-      button4.removeEventListener('click',handleFour);
-      button1.style.visibility='hidden';
-      button2.style.visibility='hidden';
-      button3.style.visibility='hidden';
-      button4.style.visibility='hidden';
-      prompt.innerHTML="Sorry, that was incorrect. The correct answer is "+question[answer];
+      button1.style.visibility='visible';
+      button2.style.visibility='visible';
+      button3.style.visibility='visible';
+      button4.style.visibility='visible';
+      contbutton.style.visibility='hidden';
+      contbutton.removeEventListener('click',cont);
+      quiz();
     }
+    contbutton.addEventListener('click',cont);
+    contbutton.style.visibility='visible';
+    button1.removeEventListener('click',handleOne);
+    button2.removeEventListener('click',handleTwo);
+    button3.removeEventListener('click',handleThree);
+    button4.removeEventListener('click',handleFour);
+    button1.style.visibility='hidden';
+    button2.style.visibility='hidden';
+    button3.style.visibility='hidden';
+    button4.style.visibility='hidden';
+    prompt.innerHTML="You got the right answer!";
   }
-  button1.addEventListener('click',handleOne);
-  button2.addEventListener('click',handleTwo);
-  button3.addEventListener('click',handleThree);
-  button4.addEventListener('click',handleFour);
+  else
+  {
+    var cont = function(event)
+  {
+    button1.style.visibility='visible';
+    button2.style.visibility='visible';
+    button3.style.visibility='visible';
+    button4.style.visibility='visible';
+    contbutton.style.visibility='hidden';
+    contbutton.removeEventListener('click',cont);
+    quiz();
+  }
+  contbutton.addEventListener('click',cont);
+  contbutton.style.visibility='visible';
+  button1.removeEventListener('click',handleOne);
+  button2.removeEventListener('click',handleTwo);
+  button3.removeEventListener('click',handleThree);
+  button4.removeEventListener('click',handleFour);
+  button1.style.visibility='hidden';
+  button2.style.visibility='hidden';
+  button3.style.visibility='hidden';
+  button4.style.visibility='hidden';
+  prompt.innerHTML="Sorry, that was incorrect. The correct answer is "+question[answer]+".";
+}
+}
+button1.addEventListener('click',handleOne);
+button2.addEventListener('click',handleTwo);
+button3.addEventListener('click',handleThree);
+button4.addEventListener('click',handleFour);
 }
 quiz();
--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 var questions=
 [
 ["How do you say hello in spanish?","Bonjour","sup","yo","hola",4],
-["How do you win a hackathon?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4],
-["How do you win money?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4],
-["How do you eat food?","Fork git projects","drink coffee","drink mountain dew","be nicholas",4]
+["How does a plant make food?","chemotaxis","photosynthesis","cellular respiration","cellular despiration",2],
+["What is the capital of California?","LA","Sacremento","Washington,D.C.","Montana",2],
+["What language is used to style a website?","CSS","HTML","Javascript","Java",1]
 ];
